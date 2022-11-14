d9aa4fed4644751291f8c065ca2a6f119532851b
olzh2102 <olzhas2102@yahoo.com>
2022-11-14 15:21:34 +0100

    fix: typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import useIsAudioActive from './hooks/use-is-audio-active';
 import useScreen from './hooks/use-screen';
-import useMediaScream from './hooks/use-media-stream';
-export { useIsAudioActive, useScreen, useMediaScream };
+import useMediaStream from './hooks/use-media-stream';
+export { useIsAudioActive, useScreen, useMediaStream };
